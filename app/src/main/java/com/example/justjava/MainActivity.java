031fd2fbaa173e26e703eedf96b401e2d88891ad
vaibhavmangla <vaibhav.mangla9@gmail.com>
2019-01-31 01:20:44 +0530

    addded mail functions

--- app/src/main/java/com/example/justjava/MainActivity.java
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -9,11 +9,15 @@
 package com.example.justjava;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
-import java.text.NumberFormat;
+import android.widget.Toast;
 
 /**
  * This app displays an order form to order coffee.
@@ -31,20 +35,45 @@ public class MainActivity extends AppCompatActivity {
      */
     int coffee=2;
     public void submitOrder(View view) {
-        String s="total price  = "+coffee*5;
-        displaymessage(s);
-        String k="total coffees = "+coffee;
+        int price=coffee*5;
+        CheckBox checkBox= findViewById(R.id.checkbox);
+        boolean ans=checkBox.isChecked();
+        CheckBox checkBox1= findViewById(R.id.checkbox1);
+        boolean choco=checkBox1.isChecked();
+        EditText name=  findViewById(R.id.yourname);
+        String fname=name.getText().toString();
+        if(ans)
+        price=price+coffee;
+        if(choco)
+            price=price+coffee*2;
+        String s="Name: "+fname+"\nAdd wipped cream? "+ans+"\nAdd chocolate: "+choco+"\nQuantity :" +coffee+"\ntotal : "+price+"\nThank you!";
+//        displaymessage(s);
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto: "));
+        intent.putExtra(Intent.EXTRA_SUBJECT,"just java order for "+fname);
+        intent.putExtra(Intent.EXTRA_TEXT,""+s);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
     }
-    private void displaymessage(String message) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(message);
+//    private void displaymessage(String message) {
+//        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+//        priceTextView.setText(message);
     }
     public void increment(View view) {
+        if(coffee==100) {
+            Toast.makeText(this,"You cannot have more Coffees",Toast.LENGTH_SHORT).show();
+            return;
+        }
         coffee++;
         display(coffee);
     }
     public void decrement(View view)
     {
+        if(coffee==1) {
+            Toast.makeText(this,"You cannot have 0 Coffees",Toast.LENGTH_SHORT).show();
+            return;
+        }
         coffee--;
         display(coffee);
     }
@@ -58,8 +87,5 @@ public class MainActivity extends AppCompatActivity {
     /**
      * This method displays the given price on the screen.
      */
-    private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+
     }
-}
